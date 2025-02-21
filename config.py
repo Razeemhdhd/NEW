e236c89c8e90738c9856207e807adcfef3dab847
Razeemhdhd <bhatroshni050@gmail.com>
2025-02-21 13:32:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY @BLURYF4CE
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7239021384:AAH2nNKrkZu8VLwxjBdMUjYC4kyJpYYIOvE")
+BOT_TOKEN=("8049258497:AAFmFCgOFRX1WGCMesppBcOcAruGtzqYy-s")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@spec890i")
+OWNER_USERNAME=("@TG_ROHAN1")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("7221087191" ,"5736732360")
+ADMIN_IDS=("7541279033" ,"7702119573")
 
 
 
